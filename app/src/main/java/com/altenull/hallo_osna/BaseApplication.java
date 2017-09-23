359e2decd2d91cbce180cb90bdef1d4e980a0578
altenull <altenull@gmail.com>
2017-09-23 14:18:43 +0900

    changed name(CustomStartApp to BaseApplication)

--- app/src/main/java/com/altenull/hallo_osna/BaseApplication.java
+++ app/src/main/java/com/altenull/hallo_osna/BaseApplication.java
@@ -8,7 +8,6 @@ import com.tsengvn.typekit.Typekit;
 public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
-
         Typekit.getInstance().addNormal(Typekit.createFromAsset(this, "SeoulNamsanM.ttf")).addBold(Typekit.createFromAsset(this, "SeoulNamsanEB.ttf"));
     }
 }
